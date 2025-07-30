4e6e001c4418c6b8b670335adebe20336678c878
Soham <77290684+SohamGanmote@users.noreply.github.com>
2025-07-30 22:53:39 +0530

    Update constants.js

--- server/config/constants.js
+++ server/config/constants.js
@@ -1,7 +1,7 @@
 module.exports = {
 	PORT: 3000,
 	PROPERTIES_PATH:
-		"/home/fezzy/shared-drive/minecraft/minecraft/server.properties",
-	SERVER_DIR: "/home/fezzy/shared-drive/minecraft/minecraft",
+		"/home/fezzy/shared-drive/Projects/pixel-host/minecraft/server.properties",
+	SERVER_DIR: "/home/fezzy/shared-drive/Projects/pixel-host/minecraft",
 	SERVER_JAR: "server.jar",
 };
